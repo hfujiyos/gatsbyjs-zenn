aefbf9487eaf79248b3f3357e13925e48f338ef3
hfujiyos <hfujiyos@gmail.com>
2022-07-24 18:38:13 +0900

    #13 testGraphQL

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,15 @@
 /**
  * Configure your Gatsby site with this file.
  *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
-module.exports = {
+ module.exports = {
+  siteMetadata: {
+    title: "Dev Blog",
+    description: "Gatsbyで作成したブログサイトです。",
+    author: "Engineer X"
+  },
   /* Your site config here */
   plugins: [],
 }
